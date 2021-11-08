d32d406d46a87527f199d06764a6538192107417
Kacper Satora <kacpersatora02@outlook.com>
2021-11-08 14:47:40 +0100

    fixed AssetController

--- API/Controllers/AssetController.cs
+++ API/Controllers/AssetController.cs
@@ -26,9 +26,9 @@ namespace API.Controllers
 
         //TODO: Implement on working DB
 
-        // [HttpGet("{id}")]
-        // public async Task<ActionResult<List<Asset>>> GetAsset(Guid id) {
-        //     return await _context.Asset.FindAsync(id);
-        // }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Asset>> GetAsset(Guid id) {
+            return await _context.Asset.FindAsync(id);
+        }
     }
 }
