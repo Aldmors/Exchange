b78f766cb275bbbd7424e1e622ae1745732fbb17
Kacper Satora <kacpersatora02@outlook.com>
2021-11-15 13:55:27 +0100

    CorsPolicy api fix

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,11 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
