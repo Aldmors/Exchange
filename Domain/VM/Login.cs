83b303db662453c7a617f4c662a821d08b822ba5
Aldmor <kubziom.pr@gmail.com>
2021-11-17 22:21:25 +0100

    Add login and register api

---
+++ Domain/VM/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.VM
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Domain/VM/Register.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.VM
+{
+    public class Register
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
